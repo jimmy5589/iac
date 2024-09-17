01f4e86ee0194737818cf2bc40cec77962974482
jimmy5589 <jimmy5589@users.noreply.github.com>
2024-09-17 19:42:07 +0700

    First configuration

---
+++ main.tf
@@ -0,0 +1,18 @@
+
+module "my_vcn01" {
+	source = "./vcn_module"
+        compartment_id = var.compartment_ocid
+        cidr_block = var.vcn01_cidr_block
+        dns_label = var.vcn01_display_name
+        display_name = var.vcn01_display_name
+}
+
+
+module "my_vcn02" {
+        source = "./vcn_module"
+        compartment_id = var.compartment_ocid
+        cidr_block = "172.16.0.0/16"
+        dns_label = "vcn02"
+        display_name = "vcn02"
+}
+
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "private_key_path" {}
+variable "fingerprint" {}
+variable "region" {}
+
+variable "compartment_ocid" {}
+variable "vcn01_cidr_block" {}
+variable "vcn01_display_name" {}
